a0b0183c543839787c52aae142392d7175927db9
Aleksandr Ignatyev <vigntom@gmail.com>
2024-10-12 13:11:23 +0300

    Format main.go and add notes to README.md

--- main.go
+++ main.go
@@ -1,72 +1,72 @@
 package main
 
 import (
-  "fmt"
-  "time"
+	"fmt"
+	"time"
 
-  "github.com/golang-collections/collections/queue"
-  "github.com/google/uuid"
+	"github.com/golang-collections/collections/queue"
+	"github.com/google/uuid"
 
-  "cube/node"
-  "cube/task"
-  "cube/manager"
-  "cube/worker"
+	"cube/manager"
+	"cube/node"
+	"cube/task"
+	"cube/worker"
 )
 
 func main() {
-  t := task.Task{
-    ID: uuid.New(),
-    Name: "Task-1",
-    State: task.Pending,
-    Image: "image-1",
-    Memory: 1024,
-    Disk: 1,
-  }
+	t := task.Task{
+		ID:     uuid.New(),
+		Name:   "Task-1",
+		State:  task.Pending,
+		Image:  "image-1",
+		Memory: 1024,
+		Disk:   1,
+	}
 
-  te := task.TaskEvent{
-    ID: uuid.New(),
-    State: task.Pending,
-    Timestamp: time.Now(),
-    Task: t,
-  }
+	te := task.TaskEvent{
+		ID:        uuid.New(),
+		State:     task.Pending,
+		Timestamp: time.Now(),
+		Task:      t,
+	}
 
-  fmt.Printf("task: %v\n", t)
-  fmt.Printf("task event: %v\n", te)
+	fmt.Printf("task: %v\n", t)
+	fmt.Printf("task event: %v\n", te)
 
-  w := worker.Worker{
-    Name: "worker-1",
-    Queue: *queue.New(),
-    Db: make(map[uuid.UUID]*task.Task),
-  }
+	w := worker.Worker{
+		Name:  "worker-1",
+		Queue: *queue.New(),
+		Db:    make(map[uuid.UUID]*task.Task),
+	}
 
-  fmt.Printf("worker: %v\n", w)
+	fmt.Printf("worker: %v\n", w)
 
-  w.CollectStats()
-  w.RunTask()
-  w.StartTask()
-  w.StopTask()
+	w.CollectStats()
+	w.RunTask()
+	w.StartTask()
+	w.StopTask()
 
-  m := manager.Manager{
-    Pending: *queue.New(),
-    TaskDb: make(map[string][]*task.Task),
-    EventDb: make(map[string][]*task.TaskEvent),
-    Workers: []string{w.Name},
-  }
+	m := manager.Manager{
+		Pending: *queue.New(),
+		TaskDb:  make(map[string][]*task.Task),
+		EventDb: make(map[string][]*task.TaskEvent),
+		Workers: []string{w.Name},
+	}
 
-  fmt.Printf("manager: %v\n", m)
+	fmt.Printf("manager: %v\n", m)
 
-  m.SelectWorker()
-  m.UpdateTasks()
-  m.SendWork()
+	m.SelectWorker()
+	m.UpdateTasks()
+	m.SendWork()
 
-  n := node.Node{
-    Name: "Node-1",
-    Ip: "192.168.1.1",
-    Cores: 4,
-    Memory: 1024,
-    Disk: 25,
-    Role: "worker",
-  }
+	n := node.Node{
+		Name:   "Node-1",
+		Ip:     "192.168.1.1",
+		Cores:  4,
+		Memory: 1024,
+		Disk:   25,
+		Role:   "worker",
+	}
 
-  fmt.Printf("node: %v\n", n)
+	fmt.Printf("node: %v\n", n)
 }
